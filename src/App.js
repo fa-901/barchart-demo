a1823da8a1b79f301ae5fbdfeb0e95e8a4a49a0d
Farhan Alam <farhan_w901@outlook.com>
2020-09-28 23:35:11 +0600

    add react-select to slect data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import React, { useEffect, useState } from "react";
 import "./styles/styles.css";
 import "./styles/tip.css";
 import ChartComp from './ChartComp';
-import json from './data/data.json'
+import json from './data/data.json';
+import Select from 'react-select';
 
 export default function App() {
 	const [dType, update] = useState('daily');
+	var options = [
+		{ label: 'Daily', value: 'daily' },
+		{ label: 'Weekly', value: 'weekly' },
+		{ label: 'Monthly', value: 'monthly' },
+	]
 
 	return (
 		<div className="App">
 			<div className="container card my-3 p-3">
-				<h4>Chart Demo</h4>
+				<div className='d-flex'>
+					<div className=''>
+						<h4>Chart Demo</h4>
+					</div>
+					<div className='ml-auto w-25'>
+						<Select
+							placeholder='Select Data'
+							options={options}
+							onChange={(v) => { update(v.value) }}
+							value={options.filter((v) => v.value === dType)}
+						/>
+					</div>
+				</div>
 				<ChartComp data={json[dType]} />
 			</div>
 		</div>
