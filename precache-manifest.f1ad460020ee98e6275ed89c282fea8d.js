9c951def03d05b4d4f829c93d216364472ad1345
Farhan Alam <farhan_w901@outlook.com>
2020-10-20 18:08:05 +0600

    Updates

---
+++ precache-manifest.f1ad460020ee98e6275ed89c282fea8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c59f4bfc595d8fd1ced44970ae0b7a4",
+    "url": "/barchart-demo/index.html"
+  },
+  {
+    "revision": "05320c900def0a46e1f7",
+    "url": "/barchart-demo/static/css/main.b0577052.chunk.css"
+  },
+  {
+    "revision": "3cd31fd5f98de03c2627",
+    "url": "/barchart-demo/static/js/2.dbf6212b.chunk.js"
+  },
+  {
+    "revision": "05320c900def0a46e1f7",
+    "url": "/barchart-demo/static/js/main.26571356.chunk.js"
+  },
+  {
+    "revision": "5a5df3f487cab3218dfe",
+    "url": "/barchart-demo/static/js/runtime~main.13c00841.js"
+  }
+]);
